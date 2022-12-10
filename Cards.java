d1788f54784dc2ef6b1805643eda44fc43d26669
idil toprakale <idiltkale@gmail.com>
2022-12-10 19:39:25 +0300

    i forgot it can't be pişti in the first turn I'll try to fix that later

--- Cards.java
+++ Cards.java
@@ -4,8 +4,14 @@ public class Cards {
     public static String ranks[] = { "A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K" };
     public static String mycards[] = new String[4];
     public static String yourcards[] = new String[4];
-    public static String board[] = new String[4];
+    public static String board[] = new String[52];
+    public static String onBoard;
     String deste[] = new String[52];
+    public static int pointMe = 0;
+    public static int pointYou = 0;
+    public static int pointDesk = 0;
+    public static int shdhs;
+
     public static int counter = 0;
     public static int counter2 = 1;
 
@@ -42,18 +48,21 @@ public class Cards {
         for (int i = 0; i < 4; i++) {
             if (counter % 2 == 0) {
                 mycards[counter / 2] = deste[counter];
+                deste[counter] = null;
             }
             counter += 2;
         }
         for (int i = 0; i < 4; i++) {
             if (counter2 % 2 != 0) {
                 yourcards[(counter2 - 1) / 2] = deste[counter2];
+                deste[counter2] = null;
             }
             counter2 += 2;
         }
         int boardstart = 0;
         for (int i = 8; i <= 11; i++) {
             board[boardstart] = deste[i];
+            deste[i] = null;
             boardstart++;
         }
     }
@@ -65,8 +74,51 @@ public class Cards {
         for (int i = 0; i < 4; i++) {
             System.out.println("your " + (i + 1) + ". " + "card: " + yourcards[i]);
         }
-        for (int i = 0; i < 4; i++) {
-            System.out.println("boards " + (i + 1) + "." + "card: " + board[i]);
+        for (int i = 51; i > 0; i--) {
+            if (board[i] != null) {
+                System.out.println("board: " + board[i]);
+                break;
+            }
+        }
+    }
+
+    public void PrintDeste() {
+        for (int i = 0; i < deste.length; i++) {
+            System.out.println("deste: " + deste[i]);
+        }
+    }
+
+    public void mePlay(int choose) {
+        String firstcard = mycards[0];
+        String seccard = mycards[1];
+        String thirdcard = mycards[2];
+        String fourthcard = mycards[3];
+
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] != null) {
+                onBoard = board[i];
+            } else {
+                continue;
+            }
+        }
+        if (choose == 1) {
+            if (firstcard.charAt(1) == onBoard.charAt(1)) {
+                if (board[1] != null) {
+                    System.out.println("Pişti!!!!");
+                    pointMe += 20;
+                }
+                board[0] = null;
+                pointMe += pointDesk;
+                for (int i = 0; i < board.length; i++) {
+                    board[i] = null;
+                }
+            }
+        } else {
+            for (int i = (board.length - 1); i > 0; i--) {
+                if (board[i] != null) {
+                    board[i] = firstcard;
+                }
+            }
         }
     }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Cards cd = new Cards();
         cd.Define();
         cd.Shuffle();
         cd.Display();
-        cd.DealCards();
+        cd.DealCardsFirst();
         cd.PrintOurCards();
+        System.out.println("please choose a card");
+        int abc = sc.nextInt();
+        cd.mePlay(abc);
     }
 }
